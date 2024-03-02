f25ea12da03e5192d3cad6638ea0c396acd223ce
xchen <xchen601@gmail.com>
2024-03-02 00:47:32 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Board from './components/Board';
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>oompa</h1>
+      <Board />
+
+    </div>
+  )
+}
+
+export default App
